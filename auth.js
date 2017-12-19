7112c2d971427543131f336099f030ed176316b4
Amanda <ajhwhiz@gmail.com>
2017-12-18 17:27:54 -0800

    changing callback route again

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ module.exports = function (app, passport) {
   app.get('/auth/google',
     passport.authenticate('google',
       {
-        scope : ['profile', 'email', 'https://www.googleapis.com/auth/calendar'],
+        scope : ['email'],
         accessType: 'offline',
         prompt: 'consent'
       }
@@ -15,12 +15,19 @@ module.exports = function (app, passport) {
   );
 
   // the callback after google has authenticated the user
-  app.get('/auth/google/callback/',
+  app.get('/auth/google/callback',
     passport.authenticate('google', {
       successRedirect : '/markers',
       failureRedirect : '/login'
     })
   );
+
+  app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login', session: false }),
+    function (req, res) {
+        res.cookie('accessToken', req.user.accessToken, { expires: 0 });
+        res.redirect('/markers');
+    });
 
   //Login Wall!
   app.use((req, res, next) => {
