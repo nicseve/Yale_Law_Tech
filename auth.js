6a8ee81218e8f4e380b6c7b2ef86825aafa156e0
Amanda <ajhwhiz@gmail.com>
2017-12-18 18:05:38 -0800

    went into success redirect, so trying different one

--- auth.js
+++ auth.js
@@ -14,21 +14,21 @@ module.exports = function (app, passport) {
     )
   );
 
-  // the callback after google has authenticated the user
-  // app.get('/auth/google/callback',
-  //   passport.authenticate('google', {
-  //     successRedirect : '/markers',
-  //     failureRedirect : '/login'
-  //   })
-  // );
-
+  the callback after google has authenticated the user
   app.get('/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login', session: false }),
-    function (req, res) {
-      console.log('in g-oauth2 success redirect');
-      res.cookie('accessToken', req.user.accessToken, { expires: 0 });
-      res.redirect('/markers');
-    });
+    passport.authenticate('google', {
+      successRedirect : '/markers',
+      failureRedirect : '/login'
+    })
+  );
+
+  // app.get('/auth/google/callback',
+  //   passport.authenticate('google', { failureRedirect: '/login', session: false }),
+  //   function (req, res) {
+  //     console.log('in g-oauth2 success redirect');
+  //     res.cookie('accessToken', req.user.accessToken, { expires: 0 });
+  //     res.redirect('/markers');
+  //   });
 
   //Login Wall!
   app.use((req, res, next) => {
