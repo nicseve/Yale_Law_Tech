afe98ffbe7ee0b76356c1982b465570522fa50cb
Amanda <ajhwhiz@gmail.com>
2017-12-18 17:25:31 -0800

    changing callback route

--- auth.js
+++ auth.js
@@ -15,7 +15,7 @@ module.exports = function (app, passport) {
   );
 
   // the callback after google has authenticated the user
-  app.get('/auth/google/callback',
+  app.get('/auth/google/callback/',
     passport.authenticate('google', {
       successRedirect : '/markers',
       failureRedirect : '/login'
